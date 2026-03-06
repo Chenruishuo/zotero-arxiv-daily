3eb8ef2b3aeda3e55570816ab573954c74700292
Ruishuo Chen <102526988+Chenruishuo@users.noreply.github.com>
2026-03-06 22:38:35 +0800

    Update construct_email.py

--- src/zotero_arxiv_daily/construct_email.py
+++ src/zotero_arxiv_daily/construct_email.py
@@ -125,8 +125,8 @@ def render_email(papers:list[Paper]) -> str:
                 affiliations += ', ...'
         else:
             affiliations = 'Unknown Affiliation'
-        if "arxiv.org/pdf" in p.pdf_url:
-            p.pdf_url = p.pdf_url.replace("arxiv.org/pdf", "alphaxiv.org/abs")
+        if "arxiv.org" in p.pdf_url:
+            p.pdf_url = p.pdf_url.replace("arxiv.org", "alphaxiv.org")
         parts.append(get_block_html(p.title, authors, rate, p.tldr, p.pdf_url, affiliations))
 
     content = '<br>' + '</br><br>'.join(parts) + '</br>'
